772bd7592585474e87d5cef870e2959853b6c8ee
Grant Chan <grant.r.chan@gmail.com>
2020-06-09 16:36:46 -0500

    fixed temp folder error in web scraper

--- web_scraper.py
+++ web_scraper.py
@@ -33,29 +33,41 @@ def get_excel_data(dl_list):
 #delete temp folder if exists
 if os.path.isdir('.\\temp'):
     shutil.rmtree('.\\temp')
-    os.mkdir('.\\temp')
-else:
-    os.mkdir('.\\temp')
+
+#make temp directory
+os.mkdir('.\\temp')
+
 
 chrome_options =  webdriver.ChromeOptions()
 #suppress unnecessary logs from chrome driver
 chrome_options.add_experimental_option('excludeSwitches',['enable-logging'])
+#enable headless browsing
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
+
 spinner = Halo(text='Initializing Chrome Driver', spinner='line')
 spinner.start()
+
+#navigate to website
 driver.get('https://www.houstontx.gov/police/cs/Monthly_Crime_Data_by_Street_and_Police_Beat.htm')
-#explicit wait for page to load
+#explicit wait for page to fully load
 WebDriverWait(driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+#collect all excel files via <a> tags
 hrefs = driver.find_elements_by_xpath('//a[@href]')
 dl_list = get_excel_urls(hrefs)
 get_excel_data(dl_list)
 spinner.succeed()
 spinner.start('Downloading files')
+
+#navigate to website archive
 driver.get('https://www.houstontx.gov/police/cs/crime-stats-archives.htm')
 WebDriverWait(driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+#collect all excel files via <a> tags
 hrefs = driver.find_elements_by_xpath('//a[@href]')
 dl_list = get_excel_urls(hrefs)
 get_excel_data(dl_list)
 spinner.succeed('Download completed')
+
+#terminate web scraper
 driver.quit()
 spinner.stop()
